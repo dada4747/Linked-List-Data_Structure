f4fc15095013979e65520f8f258ece2abb10f1f7
dada4747 <adsurerahul96@gmail.com>
2021-06-10 08:09:56 +0530

    ADDED:Method to add element in LinkedList

--- src/main/java/com/bridgelabz/NewLinkList.java
+++ src/main/java/com/bridgelabz/NewLinkList.java
@@ -1,11 +1,38 @@
 package com.bridgelabz;
 
 public class NewLinkList {
-    public MyNode head;
-    public MyNode tail;
+    public INode head;
+    public INode tail;
 
     public NewLinkList() {
         this.head = head;
         this.tail = tail;
     }
+
+    public void add(INode newNode) {
+        if (this.tail == null) {
+            this.tail = newNode;
+        }
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            INode tempNode = this.head;
+            this.head = newNode;
+            this.head.setNext(tempNode);
+        }
+
+    }
+    public void printNodes() {
+        StringBuffer nodes = new StringBuffer("My Nodes: ");
+        INode tempNode = head;
+        while (tempNode.getNext() != null){
+            nodes.append(tempNode.getKey());
+            if(!tempNode.equals(tail)) nodes.append("-->");
+            tempNode = tempNode.getNext();
+        }
+        nodes.append(tempNode.getKey());
+        System.out.println(nodes);
+    }
+
+
 }
--- src/test/java/com/bridgelabz/MyNodeTest.java
+++ src/test/java/com/bridgelabz/MyNodeTest.java
@@ -6,12 +6,27 @@ import org.junit.jupiter.api.Test;
 public class MyNodeTest {
     @Test
     public void givenNumber_whenLinked_shouldReturnTrue(){
-        MyNode<Integer> firstNode = new MyNode<Integer>(56);
-        MyNode<Integer> secondNode = new MyNode<>(30);
+        Node<Integer> firstNode = new Node<Integer>(56);
+        Node<Integer> secondNode = new Node<>(30);
         firstNode.setNext(secondNode);
-        MyNode<Integer> thirdNode = new MyNode<>(70);
+        Node<Integer> thirdNode = new Node<>(70);
         secondNode.setNext(thirdNode);
         boolean res = firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
         Assertions.assertTrue(res);
     }
+    @Test
+    public void given3Numbers_whenAdded_toLinkedListShouldAdded(){
+        Node<Integer> firstNode = new Node<Integer>(70);
+        NewLinkList linkList = new NewLinkList();
+        linkList.add(firstNode);
+        Node<Integer> secondNode = new Node<Integer>(30);
+        linkList.add(secondNode);
+        Node<Integer> thirdNode = new Node<Integer>(56);
+        linkList.add(thirdNode);
+        linkList.printNodes();
+        boolean re = linkList.head.equals(thirdNode) && linkList.head.getNext().equals(secondNode) &&
+                linkList.tail.equals(firstNode);
+        Assertions.assertTrue(re);
+
+        }
 }
